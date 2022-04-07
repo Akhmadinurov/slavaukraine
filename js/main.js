1e548d9f9ddaaae6e1427ce9c1743160a5fc2ab4
Igor Akhmadinurov <akhmadinurov.igor@gmail.com>
2022-04-08 01:06:42 +0300

    main page pre release

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.slider').slick({
+	dots: true,
+	infinite: true,
+	speed: 500,
+	fade: true,
+	cssEase: 'linear',
+	adaptiveHeight: true,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	arrows: false,
+	mobileFirst: true
+
+});
